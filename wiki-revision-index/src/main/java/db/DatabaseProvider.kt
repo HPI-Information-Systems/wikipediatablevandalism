bb292c7ec0e0d20898f542058390e4258b0432a7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-05-15 16:40:34 +0200

    Add db env configs and limit connection pool size

--- wiki-revision-index/src/main/java/db/DatabaseProvider.kt
+++ wiki-revision-index/src/main/java/db/DatabaseProvider.kt
@@ -9,13 +9,22 @@ object DatabaseProvider {
     private val HOST = System.getenv("HOST") ?: "localhost"
     private val PORT = System.getenv("PORT") ?: "5432"
     private val SCHEMA = System.getenv("SCHEMA") ?: "postgres"
+    private val USER = System.getenv("USER") ?: ""
+    private val PASSWORD = System.getenv("PASSWORD") ?: ""
 
     private val dataSource: HikariDataSource by lazy {
         val url = "jdbc:postgresql://$HOST:$PORT/$SCHEMA"
         println("Initializing datasource with: $url")
 
-        val config = HikariConfig().apply { jdbcUrl = url }
-        HikariDataSource(config).also { it.connection.prepareStatement(CREATE_TABLE).execute() }
+        val config = HikariConfig().apply {
+            jdbcUrl = url
+            username = USER
+            password = PASSWORD
+            maximumPoolSize = maxConnectionPoolSize()
+        }
+        HikariDataSource(config).also {
+            it.connection.prepareStatement(CREATE_TABLE).execute()
+        }
     }
 
     fun connection(): Connection = dataSource.connection
