16d23febea38386489e51e85e6a9b1a6b631bb2e
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-05-17 18:00:03 +0200

    Limit jdbc connections

--- wiki-revision-index/src/main/java/db/DatabaseUtils.kt
+++ wiki-revision-index/src/main/java/db/DatabaseUtils.kt
@@ -4,4 +4,4 @@ package db
  * Connection pool size calculation suggested by PostgreSQL:
  * @see <a href="https://wiki.postgresql.org/wiki/Number_Of_Database_Connections">Number Of Database Connections</a>
  */
-fun maxConnectionPoolSize() = 2 * Runtime.getRuntime().availableProcessors()
+fun maxConnectionPoolSize() = Runtime.getRuntime().availableProcessors()
