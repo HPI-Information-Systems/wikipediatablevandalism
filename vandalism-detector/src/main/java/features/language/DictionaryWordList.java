f3be1cff4af37fc117be37ee454860e23f9d2f07
Falco Duersch <falco.duersch@student.hpi.de>
2018-07-17 10:20:10 +0200

    Add origin of English word list

--- vandalism-detector/src/main/java/features/language/DictionaryWordList.java
+++ vandalism-detector/src/main/java/features/language/DictionaryWordList.java
@@ -3,6 +3,11 @@ package features.language;
 import java.util.Set;
 import util.WordListUtil;
 
+/**
+ * List of English dictionary words.
+ *
+ * @see <a href="https://github.com/dwyl/english-words">GH: dwyl/english-words</a>
+ */
 class DictionaryWordList {
 
   private static final String FILENAME = "english-words.txt";
