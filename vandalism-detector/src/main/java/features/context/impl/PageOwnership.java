2e1e76d6941c7a55bc8699408b8c958f500a8dcd
Falco Duersch <falco.duersch@student.hpi.de>
2018-07-31 16:45:56 +0200

    Add page ownership (author rank)

---
+++ vandalism-detector/src/main/java/features/context/impl/PageOwnership.java
@@ -0,0 +1,62 @@
+package features.context.impl;
+
+import static java.util.stream.Collectors.toList;
+
+import features.Feature;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import model.FeatureParameters;
+import org.sweble.wikitext.dumpreader.export_0_10.ContributorType;
+import util.BasicUtils;
+import wikixmlsplit.datastructures.MyRevisionType;
+
+public class PageOwnership implements Feature {
+
+  @Override
+  public double getValue(FeatureParameters parameters) {
+    final ContributorType currentContributor = parameters.getRevision().getContributor();
+
+    if (BasicUtils.isAnonymous(currentContributor)) {
+      return -1;
+    }
+
+    final Map<String, Integer> contributionCounts = contributionCounts(parameters);
+    final int myContributions = contributionCounts
+        .getOrDefault(currentContributor.getUsername(), 0);
+
+    if (myContributions == 0) {
+      return 0;
+    }
+
+    final List<Integer> ranks = contributionCounts.values()
+        .stream()
+        .distinct()
+        .sorted()
+        .collect(toList());
+
+    return ranks.indexOf(myContributions);
+  }
+
+  private Map<String, Integer> contributionCounts(final FeatureParameters parameters) {
+    final Map<String, Integer> editCounts = new HashMap<>();
+
+    for (final MyRevisionType revision : parameters.getPreviousRevisions()) {
+
+      if (BasicUtils.isAnonymous(revision.getContributor())) {
+        continue;
+      }
+
+      final String username = revision.getContributor().getUsername();
+      editCounts.compute(username, (name, counter) -> {
+        if (counter == null) {
+          return 1;
+        }
+
+        return counter + 1;
+      });
+    }
+
+    return editCounts;
+  }
+}
---
+++ vandalism-detector/src/test/java/features/context/impl/PageOwnershipTest.java
@@ -0,0 +1,107 @@
+package features.context.impl;
+
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import lombok.val;
+import model.FeatureParameters;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sweble.wikitext.dumpreader.export_0_10.ContributorType;
+import wikixmlsplit.datastructures.MyRevisionType;
+
+class PageOwnershipTest {
+
+  private static final String ANONYMOUS = null;
+  private static final String BOB = "bob";
+  private static final String ALICE = "alice";
+  private static final String MALICE = "malice";
+
+  private PageOwnership pageOwnership;
+
+  @BeforeEach
+  void setUp() {
+    pageOwnership = new PageOwnership();
+  }
+
+  @Test
+  void anonymous_hasLowestRankPossible() {
+    val params = params(ANONYMOUS);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isEqualTo(-1);
+  }
+
+  @Test
+  void firstEditOfCurrentUser_historyEmpty() {
+    val params = params(BOB);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isZero();
+  }
+
+  @Test
+  void firstEditOfCurrentUser_historyNotEmpty() {
+    val params = params(BOB,
+        ALICE);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isZero();
+  }
+
+  @Test
+  void pageNotOwned() {
+    val params = params(BOB,
+        ALICE, ALICE, ALICE);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isZero();
+  }
+
+  @Test
+  void pageOwned() {
+    val params = params(BOB,
+        BOB, ALICE, BOB);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isOne();
+  }
+
+  @Test
+  void someAuthorsOfSameRank() {
+    val params = params(BOB,
+        MALICE, MALICE, ALICE, ALICE, BOB, BOB, BOB, BOB, ANONYMOUS);
+
+    val value = pageOwnership.getValue(params);
+
+    assertThat(value).isEqualTo(1);
+  }
+
+  private FeatureParameters params(final String currentAuthor, final String... previousAuthors) {
+    return FeatureParameters.builder()
+        .revision(revisionOf(currentAuthor))
+        .previousRevisions(Stream.of(previousAuthors).map(this::revisionOf).collect(toList()))
+        .build();
+  }
+
+  private MyRevisionType anonymous() {
+    return new MyRevisionType() {{
+      contributor = new ContributorType();
+    }};
+  }
+
+  private MyRevisionType revisionOf(final String author) {
+    return new MyRevisionType() {{
+      contributor = new ContributorType();
+      contributor.setUsername(author);
+    }};
+  }
+
+}
