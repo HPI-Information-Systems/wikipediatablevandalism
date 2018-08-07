b0a06f487cb138f69cbd2c8ad2773d004e965095
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-08-07 17:10:00 +0200

    Add new single label classifier

--- vandalism-classifier/classifier/single_label_classifier.py
+++ vandalism-classifier/classifier/single_label_classifier.py
@@ -4,15 +4,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate, cross_val_predict
 
 from evaluation.utils import log_scores
-from preprocessing.preprocessor import Output
+from preprocessing.preprocessor import DataSet
 
 logger = logging.getLogger()
 
 
 class SingleLabelClassifier:
 
-    def __init__(self, output: Output, trees: int, k_folds=10):
-        self.output = output
+    def __init__(self, dataset: DataSet, trees: int, k_folds=10):
+        self.dataset = dataset
         self.trees = trees
         self.k_folds = k_folds
         self.clf = RandomForestClassifier(n_estimators=self.trees, n_jobs=-1)
@@ -20,7 +20,7 @@ class SingleLabelClassifier:
     def cross_validate(self):
         logger.debug('Training single label classifier with %d trees on %d folds', self.trees, self.k_folds)
         self.clf = RandomForestClassifier(n_estimators=self.trees, n_jobs=-1)
-        scores = cross_validate(self.clf, self.output.X_train, self.output.y_train,
+        scores = cross_validate(self.clf, self.dataset.X_train, self.dataset.y_train,
                                 scoring=['f1', 'precision', 'recall', 'roc_auc', 'accuracy'],
                                 cv=self.k_folds, n_jobs=-1, return_train_score=False)
         log_scores(scores)
@@ -29,19 +29,19 @@ class SingleLabelClassifier:
     def train_predict(self):
         logger.debug('Retrieving probabilities with %d trees on %d folds', self.trees, self.k_folds)
         self.clf = RandomForestClassifier(n_estimators=self.trees, n_jobs=-1)
-        classes = cross_val_predict(self.clf, self.output.X_train, self.output.y_train,
+        classes = cross_val_predict(self.clf, self.dataset.X_train, self.dataset.y_train,
                                     cv=self.k_folds, n_jobs=-1, method='predict')
-        probabilities = cross_val_predict(self.clf, self.output.X_train, self.output.y_train,
+        probabilities = cross_val_predict(self.clf, self.dataset.X_train, self.dataset.y_train,
                                           cv=self.k_folds, n_jobs=-1, method='predict_proba')
         return classes, probabilities
 
     def test_predict(self):
         logger.debug('Retrieving probabilities with %d trees on %d folds', self.trees, self.k_folds)
         self.clf = RandomForestClassifier(n_estimators=self.trees, n_jobs=-1)
-        self.clf.fit(self.output.X_train, self.output.y_train)
+        self.clf.fit(self.dataset.X_train, self.dataset.y_train)
 
-        classes = self.clf.predict(self.output.X_test)
-        probabilities = self.clf.predict_proba(self.output.X_test)
+        classes = self.clf.predict(self.dataset.X_test)
+        probabilities = self.clf.predict_proba(self.dataset.X_test)
         return classes, probabilities
 
     def feature_importance(self):
