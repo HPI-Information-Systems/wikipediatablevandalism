c210dad64c36780225a5f3293cc1edb0f581f9b7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2018-08-09 17:00:20 +0200

    Add modules for model persistence

---
+++ vandalism-classifier/plotting.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve, average_precision_score, auc, roc_curve
+
+def plot_confusion_matrix(y_true, y_pred):
+    vandalism_count = y_true.value_counts()[True]
+    no_vandalism_count = y_true.value_counts()[False]
+    cfn_matrix = confusion_matrix(y_true, y_pred)
+    cfn_norm_matrix = np.array([[1.0/no_vandalism_count, 1.0/no_vandalism_count], [1.0/vandalism_count, 1.0/vandalism_count]])
+    norm_cfn_matrix = cfn_matrix * cfn_norm_matrix
+    
+    fig = plt.figure(figsize=(15,5))
+    ax = fig.add_subplot(1,2,1)
+    sns.heatmap(cfn_matrix, annot=True, fmt="d", linewidths=0.5, ax=ax)
+    plt.title('Confusion Matrix')
+    plt.ylabel('Real Classes')
+    plt.xlabel('Predicted Classes')
+
+    ax = fig.add_subplot(1,2,2)
+    sns.heatmap(norm_cfn_matrix, linewidths=0.5, annot=True, ax=ax)
+    plt.title('Normalized Confusion Matrix')
+    plt.ylabel('Real Classes')
+    plt.xlabel('Predicted Classes')
+    plt.show()
+    
+    print('Classification Report')
+    classes = ['No Vandalism', 'Vandalism']
+    print(classification_report(y_true, y_pred, target_names=classes))
+    
+def plot_scores(scores):
+    print('Cross validation scores')
+    scores = list(scores.items())[2:]
+
+    for score in scores:
+        sns.lineplot(range(0, 10), score[1], label=score[0])
+        plt.legend(bbox_to_anchor=(1.1, 1.05))
+    plt.show()
+
+def plot_precision_recall(y_true, y_predict_proba):
+    precision, recall, thresholds = precision_recall_curve(y_true, y_predict_proba)
+    average_precision = average_precision_score(y_true, y_predict_proba)
+    pr_auc = auc(recall, precision)
+
+    plt.title('Precision-Recall: Average Precision=  %0.2f' % average_precision)
+    plt.step(recall, precision, color='b', where='post')
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.ylabel('Precision')
+    plt.xlabel('Recall')
+    plt.show()
+    print('Precision / Recall AUC', pr_auc)
+    
+def plot_roc(y_true, y_predict_proba):
+    false_positive_rate, true_positive_rate, thresholds = roc_curve(y_true, y_predict_proba)
+    roc_auc = auc(false_positive_rate, true_positive_rate)
+
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(false_positive_rate, true_positive_rate, 'b', label='AUC = %0.2f' % roc_auc)
+    plt.legend(loc='lower right')
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+    print('Receiver Operation Characteristic AUC', roc_auc)
